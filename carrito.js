8773bd33c89f9dadc4559ef7b1360e310503a1dc
Exequiel Sosa <exequielsosa@gmail.com>
2023-07-12 19:17:45 -0300

    api 3

--- carrito.js
+++ carrito.js
@@ -1,13 +1,25 @@
 import productos from "./js/productos.js";
 
-const carritoFrutas = [];
+const almacenarCarrito = () => {
+  if (carritoFrutas.length > 0) {
+    localStorage.setItem("carritoFrutas", JSON.stringify(carritoFrutas));
+  }
+};
+
+const recuperarCarrito = () => {
+  const carritoGuardado = localStorage.getItem("carritoFrutas");
+  return carritoGuardado ? JSON.parse(carritoGuardado) : [];
+};
+
+export const carritoFrutas = recuperarCarrito();
 
 function agregarAlCarrito(frutald) {
   if (frutald > 0) {
     const producto = productos.find((p) => p.id === Number(frutald));
     if (producto !== undefined) {
       carritoFrutas.push(producto);
-      console.table(carritoFrutas);
+      console.table(carritoFrutas);      
+      almacenarCarrito();
     }
   }
 }
